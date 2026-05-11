8e93ef16d190dc44ced9dd6f88d3831a0a3f1b6a
Vika <vikababyuk15@gmail.com>
2026-05-11 19:38:44 +0300

    edited2

--- src/middleware/notFoundHandler.js
+++ src/middleware/notFoundHandler.js
@@ -1,6 +1,11 @@
-export const errorHandler = (req, res, next) => {
-  res.status(500).json({
-    message: 'Internal Server Error',
-  });
 
+
+export const errorHandler = (err, req, res, next) => {
+  const status = err.status || 500;
+
+  res.status(status).json({
+    status: status,
+    message: err.message || 'Internal Server Error',
+    data: err.data || null,
+  });
 };
