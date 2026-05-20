276de079d42ac91229125d521e3871230ff4dda1
Vika <vikababyuk15@gmail.com>
2026-05-20 22:49:38 +0300

    hm

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -5,14 +5,20 @@ import { createNote } from '../controllers/notesController.js';
 import { getNoteById } from '../controllers/notesController.js';
 import { deleteNote } from '../controllers/notesController.js';
 import { updateNote } from '../controllers/notesController.js';
+import { 
+  getAllNotesSchema, 
+  createNoteSchema, 
+  noteIdSchema, 
+  updateNoteSchema 
+} from '../validations/notesValidation.js';
 
 const router = express.Router();
 
-router.get('/notes', getAllNotes);
-router.get('/notes/:noteId', getNoteById);
-router.post('/notes', createNote);
-router.delete('/notes/:noteId', deleteNote);
-router.patch('/notes/:noteId', updateNote);
+router.get('/notes', getAllNotesSchema, getAllNotes);
+router.get('/notes/:noteId', noteIdSchema, getNoteById);
+router.post('/notes', createNoteSchema, createNote);
+router.delete('/notes/:noteId', noteIdSchema, deleteNote);
+router.patch('/notes/:noteId', updateNoteSchema, updateNote);
 
 
 export default router;
