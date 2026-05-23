25067c9f15a8f9187ea7ba832aed10a2c597ea1a
Vika <vikababyuk15@gmail.com>
2026-05-23 15:23:54 +0300

    updatd

--- src/models/note.js
+++ src/models/note.js
@@ -1,16 +1,29 @@
 import {Schema, model} from 'mongoose';
 import { TAGS } from '../constants/tags.js';
 
-const mongooseSchema = new Schema ({
-title: {type: String, trim: true, required: true},
-content:{trim: true, type: String,
-default: ""},
-tag:{type: String, default:"Todo", 
-enum: TAGS},},
-{timestamps: true}
+const mongooseSchema = new Schema (
+    {
+title: {
+    type: String, 
+    trim: true, 
+    required: true
+},
+content:{
+    trim: true, 
+    type: String,
+    default: ""
+},
+tag:{
+    type: String, 
+    default:"Todo", 
+    enum: TAGS},
+},
+{
+    timestamps: true
+}
 );
 
+mongooseSchema.index({ tag: '1' });
 
-mongooseSchema.index({ title: 'text', content: 'text' });
 export const Note = model('Note', mongooseSchema);
 
