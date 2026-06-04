e2029bb20a9f581aff2b51c1538f3d2ffc514089
Vika <vikababyuk15@gmail.com>
2026-06-04 20:15:53 +0300

    Convert all models to ES Modules

--- src/models/session.js
+++ src/models/session.js
@@ -1,29 +1,13 @@
-const mongoose = require('mongoose');
+import { Schema, model } from 'mongoose';
 
-const sessionSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User', 
-  },
-  accessToken: {
-    type: String,
-    required: true,
-  },
-  refreshToken: {
-    type: String,
-    required: true,
-  },
-  accessTokenValidUntil: {
-    type: Date,
-    required: true,
-  },
-  refreshTokenValidUntil: {
-    type: Date,
-    required: true,
-  }
+const sessionSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, required: true },
+  accessToken: { type: String, required: true },
+  refreshToken: { type: String, required: true },
+  accessTokenValidUntil: { type: Date, required: true },
+  refreshTokenValidUntil: { type: Date, required: true },
 }, {
-  timestamps: true, 
+  timestamps: true,
 });
 
-export const Session = mongoose.model('Session', sessionSchema);
+export default model('Session', sessionSchema);
