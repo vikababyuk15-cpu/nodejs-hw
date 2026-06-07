96afe7525f336aec72f1c3369c02edd41282237c
Vika <vikababyuk15@gmail.com>
2026-06-07 16:21:53 +0300

    new

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import { authenticate } from '../middleware/authenticate.js';
-import { updateUserAvatar } from '../controllers/userController.js';
+import { updateUserAvatar } from '../controllers/userControllers.js';
 import { upload } from '../middleware/multer.js';
 
 const router = Router();
